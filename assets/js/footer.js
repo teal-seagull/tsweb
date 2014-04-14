32a793bfd79dc50548e50bff04d09e6a9e1baf81
Rafael J. Rodriguez <rafase282@gmail.com>
2014-04-13 23:01:51 -0400

    js shit

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,31 +1,10 @@
 function footer()
 {
-	document.write('
-	<hr class="featurette-divider">
-	
-	<hr class="featurette-divider">
-
-      <!-- /END THE FEATURETTES -->
-
-    </div>
-    <!-- FOOTER -->
-    <div class="footer-carousel">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-2 col-sm-offset-8">
-            <h5>Contact Us</h5>
-            <div>
-              hello@tealseagull.com
-            </div>
-          </div>
-          <div class="col-sm-2">
-            <h5>Social Media</h5>
-            <a href="https://www.facebook.com/tealseagull" target="_blank" ><div id="facebook"></div></a>
-            <div id="twitter"></div>
-            <a href="https://www.linkedin.com/company/teal-seagull-it-services" target="_blank" ><div id="linkedin"></div></a>
-          </div>
-        </div>
-      </div>
-    </div>
-	');
+	document.write('<hr class="featurette-divider">');
+	document.write('<!-- /END THE FEATURETTES -->');
+	document.write('</div>');
+	document.write('<!-- FOOTER --> <div class="footer-carousel"> <div class="container"> <div class="row">  <div class="col-sm-2 col-sm-offset-8"> <h5>Contact Us</h5>  <div>');
+	document.write(' hello@tealseagull.com  </div> </div> <div class="col-sm-2">  <h5>Social Media</h5> <a href="https://www.facebook.com/tealseagull" target="_blank" ><div id="facebook">');
+	document.write('</div></a> <div id="twitter"></div>  <a href="https://www.linkedin.com/company/teal-seagull-it-services" target="_blank" ><div id="linkedin"></div></a>');
+	document.write('</div> </div>  </div> </div> ');
 }
