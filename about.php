27f61ca8c185afb45b3da121fd37f154878c4667
Fernando Tong <nandorov@gmail.com>
2014-04-15 00:41:40 -0500

    changing to php
    Testing changing files to php

--- about.php
+++ about.php
@@ -107,7 +107,7 @@
     </div>
   </div> <!-- /container -->
 
-<?php include("include/footer.html"); ?>
+<?php include("include/footer.php"); ?>
 
 <script src="assets/js/jquery-1.10.2.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
